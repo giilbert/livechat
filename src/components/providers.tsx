0b56192766e5b5d96a66534564f66f9a2d7c1c7d
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-10-16 13:46:42 -0400

    configure server

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,5 +5,19 @@ import { MafProvider } from "@usemaf/react";
 export const Providers: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  return <MafProvider server="dev">{children}</MafProvider>;
+  return (
+    <MafProvider
+      server={
+        process.env.NODE_ENV === "development"
+          ? "dev"
+          : {
+              type: "platform",
+              url: "https://maf-server.fly.dev",
+              app: "gilbert/server",
+            }
+      }
+    >
+      {children}
+    </MafProvider>
+  );
 };
